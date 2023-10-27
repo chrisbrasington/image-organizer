1fbd6662e7a2c85d54e184b2dcaf8ebe2894e3dc
chrisbrasington <1186896+chrisbrasington@users.noreply.github.com>
2023-10-27 17:43:48 -0600

    remove ascii

--- program.py
+++ program.py
@@ -5,6 +5,7 @@ import sys
 # import PIL.Image
 import subprocess
 import time
+import math
 
 if len(sys.argv) != 3:
     print("Usage: python program.py <input_directory> <output_directory>")
@@ -29,29 +30,6 @@ if not os.path.exists(file_path):
 with open(file_path, 'r') as file:
     unique_paths = [line.strip() for line in file.readlines()]
 
-ASCII_CHARS = ["@", "#", "$", "%", "?", "*", "+", ";", ":", ",", "."]
-
-import math
-
-def resize(image, new_width=100):
-    old_width, old_height = image.size
-    
-    new_height = math.floor(new_width * old_height / old_width)
-    new_width = math.floor(new_width)
-
-    print(new_width, new_height)
-
-    return image.resize((new_width, new_height))
-
-def to_greyscale(image):
-    return image.convert("L")
-
-def pixel_to_ascii(image):
-    pixels = image.getdata()
-    ascii_str = "";
-    for pixel in pixels:
-        ascii_str += ASCII_CHARS[pixel//25];
-    return ascii_str
 
 # Loop through images in the input directory
 for filename in os.listdir(input_directory):
@@ -67,22 +45,6 @@ for filename in os.listdir(input_directory):
 
         time.sleep(0.8)
         subprocess.call(["wmctrl", "-a", "code"])
-
-        # #resize image
-        # image = PIL.Image.open(image_path)
-        # image = resize(image);
-        # #convert image to greyscale image
-        # greyscale_image = to_greyscale(image)
-        # # convert greyscale image to ascii characters
-        # ascii_str = pixel_to_ascii(greyscale_image)
-        # img_width = greyscale_image.width
-        # ascii_str_len = len(ascii_str)
-        # ascii_img=""
-        # #Split the string based on width  of the image
-        # for i in range(0, ascii_str_len, img_width):
-        #     ascii_img += ascii_str[i:i+img_width] + "\n"
-
-        # print(ascii_img)
 
         skip = False
 
