ea1ae318d1c787fa0397695235f7f80e74f49910
chrisbrasington <1186896+chrisbrasington@users.noreply.github.com>
2023-10-27 12:54:53 -0600

    image display

--- program.py
+++ program.py
@@ -2,11 +2,7 @@
 import os
 import shutil
 import sys
-import subprocess
-import threading
-
-def run_feh(image_path):
-    subprocess.run(["feh", image_path])
+from PIL import Image
 
 if len(sys.argv) != 3:
     print("Usage: python program.py <input_directory> <output_directory>")
@@ -28,45 +24,12 @@ unique_paths = []
 
 # Loop through images in the input directory
 for filename in os.listdir(input_directory):
-    import time
-
-    # Initialize a global variable to keep track of the feh thread
-    feh_thread = None
-
-    # Initialize a threading event to signal the feh thread to stop
-    stop_event = threading.Event()
-
-    def run_feh(image_path):
-        global stop_event
-        while not stop_event.is_set():
-            subprocess.run(["feh", image_path])
-            time.sleep(1)
-
-    if len(sys.argv) != 3:
-        print("Usage: python program.py <input_directory> <output_directory>")
-        sys.exit(1)
-
-    input_directory = sys.argv[1]
-    output_directory = sys.argv[2]
-
-    print(input_directory)
-    print(output_directory)
-
-    # Create subdirectories if they don't exist
-    os.makedirs(output_directory, exist_ok=True)
-
-    # Initialize an array to store unique paths
-    unique_paths = []
-
-    # Loop through images in the input directory
-    for filename in os.listdir(input_directory):
-        if filename.endswith(('.jpg', '.jpeg', '.png', '.gif')):
-            # open with feh on a separate thread
-            image_path = os.path.join(input_directory, filename)
-            feh_thread = threading.Thread(target=run_feh, args=(image_path,))
-            feh_thread.start()
-
+    if filename.endswith(('.jpg', '.jpeg', '.png', '.gif')):
+        # open and resize the image
         image_path = os.path.join(input_directory, filename)
+
+        with Image.open(image_path) as image:
+            image.show()
 
         # Ask the user for a file path to move the image to
         while True:
@@ -85,11 +48,6 @@ for filename in os.listdir(input_directory):
             else:
                 print("Invalid path. Please try again.")
 
-        # Kill the feh thread
-        stop_event.set()
-        if feh_thread is not None:
-            feh_thread.join()
-        
         # Move the image to the selected path
         shutil.move(image_path, os.path.join(selected_path, filename))
 
