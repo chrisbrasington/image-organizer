5358e75f32ea7833bf52e14f2f665a39fab5f6ca
chrisbrasington <1186896+chrisbrasington@users.noreply.github.com>
2023-10-27 13:28:18 -0600

    README

---
+++ README.md
@@ -0,0 +1,47 @@
+# Image Sorting Program
+
+## Overview
+
+This Python program is designed to help you organize a directory of images by moving them into subdirectories of your choice. The program reads image files from an input directory and allows you to select or enter a destination path for each image. It will then move the images accordingly.
+
+## Requirements
+
+- Python 3.x
+- Input and output directories for image organization
+
+## Usage
+
+To use this program, follow these steps:
+
+1. Open a terminal window.
+
+2. Run the program using the following command:
+
+   ```bash
+   python program.py <input_directory> <output_directory>
+   ```
+
+Replace <input_directory> with the path to the directory containing your images.
+Replace <output_directory> with the path to the directory where you want the organized images to be placed.
+
+The program will display the images in the input directory one by one and prompt you to select or enter a destination path for each image.
+
+You can select a previously used path by entering the corresponding number, or you can enter a new path. The program will create subdirectories as needed.
+
+After processing all the images, the program will display the prior paths you've used.
+
+The program also stores your user-modified paths in a file named user_modified_paths.txt for future reference.
+    
+# Note
+
+This program assumes that the input images are in the following formats: .jpg, .jpeg, .png, or .gif. Images in other formats will be ignored.
+
+Make sure to have a text file named user_modified_paths.txt in the same directory as the program to store and read the user-modified paths.
+
+Please ensure that you have appropriate permissions to read from the input directory and write to the output directory.
+
+Caution: Moving and organizing files using this program will result in files being moved to the selected output directory, and existing files with the same names may be overwritten. Use with caution.
+
+# Usage
+
+Feel free to modify and enhance this program to suit your specific needs. Enjoy organizing your images with ease!
--- program.py
+++ program.py
@@ -21,38 +21,56 @@ os.makedirs(output_directory, exist_ok=True)
 
 # Initialize an array to store unique paths
 unique_paths = []
+with open('user_modified_paths.txt', 'r') as file:
+    unique_paths = [line.strip() for line in file.readlines()]
+
+print(unique_paths)
 
 ASCII_CHARS = ["@", "#", "$", "%", "?", "*", "+", ";", ":", ",", "."]
 
-def resize(image, new_width = 100):
+import math
+
+def resize(image, new_width=100):
     old_width, old_height = image.size
-    new_height = new_width * old_height / old_width
+    
+    new_height = math.floor(new_width * old_height / old_width)
+    new_width = math.floor(new_width)
+
+    print(new_width, new_height)
+
     return image.resize((new_width, new_height))
 
 def to_greyscale(image):
     return image.convert("L")
 
+def pixel_to_ascii(image):
+    pixels = image.getdata()
+    ascii_str = "";
+    for pixel in pixels:
+        ascii_str += ASCII_CHARS[pixel//25];
+    return ascii_str
+
 # Loop through images in the input directory
-for filename in os.listdir(input_directory):
+for filename in sorted(os.listdir(input_directory)):
     if filename.endswith(('.jpg', '.jpeg', '.png', '.gif')):
         # open and resize the image
         image_path = os.path.join(input_directory, filename)      
 
-        #resize image
-        image = PIL.Image.open(image_path)
-        image = resize(image);
-        #convert image to greyscale image
-        greyscale_image = to_greyscale(image)
-        # convert greyscale image to ascii characters
-        ascii_str = pixel_to_ascii(greyscale_image)
-        img_width = greyscale_image.width
-        ascii_str_len = len(ascii_str)
-        ascii_img=""
-        #Split the string based on width  of the image
-        for i in range(0, ascii_str_len, img_width):
-            ascii_img += ascii_str[i:i+img_width] + "\n"
+        # #resize image
+        # image = PIL.Image.open(image_path)
+        # image = resize(image);
+        # #convert image to greyscale image
+        # greyscale_image = to_greyscale(image)
+        # # convert greyscale image to ascii characters
+        # ascii_str = pixel_to_ascii(greyscale_image)
+        # img_width = greyscale_image.width
+        # ascii_str_len = len(ascii_str)
+        # ascii_img=""
+        # #Split the string based on width  of the image
+        # for i in range(0, ascii_str_len, img_width):
+        #     ascii_img += ascii_str[i:i+img_width] + "\n"
 
-        print(ascii_img)
+        # print(ascii_img)
 
         # Ask the user for a file path to move the image to
         while True:
@@ -66,6 +84,12 @@ for filename in os.listdir(input_directory):
             else:
                 selected_path = os.path.join(output_directory, user_input)
                 unique_paths.append(selected_path)
+
+            unique_paths = sorted(unique_paths)
+
+            if not os.path.exists(selected_path):
+                os.makedirs(selected_path)
+
             if os.path.exists(selected_path):
                 break
             else:
@@ -82,4 +106,4 @@ for filename in os.listdir(input_directory):
     # Update user-modified paths
     with open('user_modified_paths.txt', 'w') as file:
         for i, path in enumerate(unique_paths):
-            file.write(f"{i+1}:{path}\n")
+            file.write(f"{path}\n")
